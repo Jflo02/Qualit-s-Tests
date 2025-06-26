92b1c5f8e5780d6451b26e8575a3ad688c99c141
bagroo <florimond2002@gmail.com>
2025-06-26 09:48:47 +0200

    exercice d'intro ok

---
+++ src/modules/authentication/user/user.test.js
@@ -0,0 +1,54 @@
+import { describe, it, afterEach } from "vitest";
+import { vi } from "vitest";
+import { createUser } from "./user.service";
+import { expect } from "vitest";
+import { createUserInRepository } from "./user.repository";
+
+vi.mock("./user.repository", async (importOriginal) => ({
+  ...(await importOriginal()),
+  createUserInRepository: vi.fn((data) => {
+    return {
+      id: 4,
+      name: data.name,
+      birthday: data.birthday,
+    };
+  }),
+}));
+
+describe("User Service", () => {
+
+  afterEach(() => vi.clearAllMocks())
+
+  it("should create an user", async () => {
+    const user = await createUser({
+      name: "Valentin R",
+      birthday: new Date(1997, 8, 13),
+    });
+
+    expect(user).toBeDefined();
+    expect(user.id).toBeDefined();
+    expect(user.id).toBeTypeOf("number");
+    expect(user).toHaveProperty("name", "Valentin R");
+    expect(user.birthday).toBeDefined();
+    expect(user.birthday.getFullYear()).toBe(1997);
+    expect(user.birthday.getMonth()).toBe(8);
+
+    expect(createUserInRepository).toBeCalledTimes(1);
+    expect(createUserInRepository).toBeCalledWith({
+      name: "Valentin R",
+      birthday: new Date(1997, 8, 13),
+    });
+  });
+
+  it("should trigger a bad request error when user creation", async () => {
+    try {
+      await createUser({
+        name: "Valentin R",
+      });
+      assert.fail("createUser should trigger an error.");
+    } catch (e) {
+      expect(e.name).toBe('HttpBadRequest');
+      expect(e.statusCode).toBe(400);
+    }
+  });
+});
